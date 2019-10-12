44cef824a88bcf26e5ebe71dfc89755fdb3926cc
Zakali23 <zakaria.zekraoui@gmail.com>
2019-10-13 01:29:16 +0200

    design ok

--- assets/js/components/resultats.vue
+++ assets/js/components/resultats.vue
@@ -1,56 +1,96 @@
 <template>
-    <v-card
-            class="mx-auto"
-            color="#F9F9F9"
+    <div>
+            <v-card
+                    class="mx-auto anime "
+                    color="#F9F9F9"
+            >
+                <v-card-text :class="animation">
+                    <v-list-item two-line :class="animation">
+                        <template class="text-center">
+                            <v-btn class="ma-2" tile x-large color="#F5DEB3" icon>
+                                <v-icon>mdi-map-marker</v-icon>
+                            </v-btn>
+                        </template>
 
-    >
-        <v-list-item two-line>
-            <v-list-item-content>
-                <v-list-item-title class="headline">{{ville}}</v-list-item-title>
-                <v-list-item-subtitle>{{date| moment("dddd,Do MMMM  YYYY")}}</v-list-item-subtitle>
-            </v-list-item-content>
-        </v-list-item>
+                        <v-list-item-content>
+                            <v-list-item-title class="headline">{{ville.toUpperCase()}}</v-list-item-title>
+                            <v-list-item-subtitle >{{date| moment("dddd,Do MMMM  YYYY")}}</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-row align="center">
+                        <v-col class="display-3 colory" cols="6">
+                            {{temperature}}&deg;C
+                        </v-col>
+                        <v-col cols="6">
+                            <v-img
+                                    :src="icon"
+                                    alt="Sunny image"
+                                    width="90"
+                            ></v-img>
+                            <h1 class="colory">{{description}}</h1>
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+                <v-layout wrap row>
+                   <v-flex xs3 class="text-sm-center">
+                       <v-btn text color="#F5DEB3">
+                           <span>Vent</span>
+                           <v-icon>mdi-finance</v-icon>
+                       </v-btn><br>
+                       <p class="infos-text">{{vent}} Km/h</p>
+                   </v-flex>
+                   <v-flex xs3 class="text-sm-center">
+                       <v-btn text color="#F5DEB3">
+                           <span>Humidité</span>
+                           <v-icon>mdi-google-podcast</v-icon>
+                       </v-btn><br>
+                       <p class="infos-text">{{humidity}} %</p>
+                   </v-flex>
+                   <v-flex xs3 class="text-sm-center">
+                       <v-btn text color="#F5DEB3">
+                           <span>Min</span>
+                           <v-icon>mdi-thermometer-minus</v-icon>
+                       </v-btn>
+                       <br>
+                       <p class="infos-text">{{min}} &deg;C</p>
+                   </v-flex>
+                   <v-flex xs3 class="text-sm-center">
+                       <v-btn text color="#F5DEB3 ">
+                           <span>Max</span>
+                           <v-icon>mdi-thermometer-plus</v-icon>
+                       </v-btn><br>
+                       <p class="infos-text">{{max}} &deg;C</p>
+                   </v-flex>
+                </v-layout>
 
-        <v-card-text>
-            <v-row align="center">
-                <v-col class="display-3" cols="6">
-                    {{temperature}}&deg;C
-                </v-col>
-                <v-col cols="6">
-                    <v-img
-                            :src="`http://openweathermap.org/img/wn/${icon}@2x.png`"
-                            alt="Sunny image"
-                            width="90"
-                    ></v-img>
-                    <h1>{{description}}</h1>
-                </v-col>
-            </v-row>
-        </v-card-text>
+                <v-divider></v-divider>
+                <template>
+                    <div class="text-center">
+                        <v-bottom-sheet v-model="sheet">
+                            <template v-slot:activator="{ on }">
+                                <v-btn
+                                        color="#F5DEB3"
+                                        v-on="on"
+                                >
+                                    Voir la Carte
+                                </v-btn>
+                            </template>
+                            <v-sheet class="text-center" height="500px">
+                                <maps :gps="coord"/>
+                            </v-sheet>
+                        </v-bottom-sheet>
+                    </div>
+                </template>
+                <template>
 
-        <v-list-item>
-            <v-list-item-icon>
-                <v-icon>mdi-send</v-icon>
-            </v-list-item-icon>
-            <v-list-item-subtitle>{{vent}} km/h</v-list-item-subtitle>
-        </v-list-item>
+                </template>
+                <v-divider></v-divider>
 
-        <v-list-item>
-            <v-list-item-icon>
-                <v-icon>mdi-cloud-download</v-icon>
-            </v-list-item-icon>
-            <v-list-item-subtitle>{{humidity}}%</v-list-item-subtitle>
-        </v-list-item>
-
-        <v-divider></v-divider>
-        <template>
-            <maps :gps="coord"/>
-        </template>
-        <v-divider></v-divider>
-
-        <v-card-actions>
-            <v-btn text @click="effacer">Effacer</v-btn>
-        </v-card-actions>
-    </v-card>
+                <v-card-actions>
+                    <v-btn outlined dark @click="effacer">Effacer</v-btn>
+                </v-card-actions>
+            </v-card>
+    </div>
 </template>
 <script>
     import store from '../stores/store'
@@ -71,24 +111,26 @@
             },
             coord:Array,
             vent:Number,
-            humidity:Number
+            humidity:Number,
+            min:Number,
+            max:Number
         },
         data () {
             return {
                 date:new Date().toISOString(),
-                labels: ['SU', 'MO', 'TU', 'WED', 'TH', 'FR', 'SA'],
-                time: 0,
-                forecast: [
-                    { day: 'Tuesday', icon: 'mdi-white-balance-sunny', temp: '24\xB0/12\xB0' },
-                    { day: 'Wednesday', icon: 'mdi-white-balance-sunny', temp: '22\xB0/14\xB0' },
-                    { day: 'Thursday', icon: 'mdi-cloud', temp: '25\xB0/15\xB0' },
-                ],
+                animation:"animated fadeInDown",
+                anime:'',
+                sheet: false
             }
         },
         methods:{
             effacer(){
                 store.commit('setShow',false)
+                this.anime = "animated fadeInUp"
             }
         }
     }
 </script>
+<style scoped>
+
+</style>
--- assets/js/components/search.vue
+++ assets/js/components/search.vue
@@ -2,8 +2,7 @@
     <v-app id="inspire" >
         <v-card flat>
             <v-toolbar
-                    color="elevation-0"
-                    dark
+                    color="transparent"
                     extended
                     flat
 
@@ -31,22 +30,45 @@
                             hide-details
                             label="Saisissez votre ville "
                             solo-inverted
+                            color="white"
 
-
-                    ></v-autocomplete>
+                    ></v-autocomplete >
                     <v-btn icon>
-                        <v-icon @click="showResult">mdi-magnify</v-icon>
+                        <v-icon @click.prevent="showResult" color="#F5DEB3">mdi-magnify</v-icon>
                     </v-btn>
 
                 </v-toolbar>
 
                 <v-divider></v-divider>
-
-                <v-card-text style="height: auto;" v-if="show">
-                <resultats :ville="commune.name" :temperature="commune.temp" :description="commune.desc" :icon="commune.icon" :vent="commune.vent" :humidity="commune.humidity" :coord="commune.coord"/>
+                <transition name="fade">
+                <v-card-text style="height: auto;" v-if="show" >
+                <resultats :ville="commune.name"
+                           :temperature="commune.temp"
+                           :description="commune.desc"
+                           :icon="commune.icon"
+                           :vent="commune.vent"
+                           :humidity="commune.humidity"
+                           :coord="commune.coord"
+                           :min="commune.min"
+                           :max="commune.max"/>
                 </v-card-text>
+                </transition>
             </v-card>
         </v-card>
+        <v-snackbar
+                v-model="snackbar"
+                bottom
+
+        >
+            {{ error }}
+            <v-btn
+                    color="red"
+                    text
+                    @click="snackbar = false"
+            >
+                Fermer
+            </v-btn>
+        </v-snackbar>
     </v-app>
 </template>
 <script>
@@ -66,12 +88,16 @@
                 vent:null,
                 desc:null,
                 humidity:null,
-                icon:null,
-                coord:[]
+                icon:'01d',
+                coord:[],
+                min:null,
+                max:null
             },
             villes:[],
             loading:false,
             search:null,
+            error:"Malheureusement, la ville demandée n'est pas disponible  ",
+            snackbar:false
 
 
            }
@@ -93,33 +119,46 @@
                      self.villes = response.data
 
                 }).catch(function (error) {
-                    console.log(error.response);
+
                 }).finally(function () {
                     self.loading = false
                 });
             },
             showResult() {
-                console.log('test')
                 const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.commune.name}&lang=fr&units=metric&appid=aeaa755359f4760cc69052fa33cbb13c`
-                let self = this
-                this.axios({
-                    method: 'get',
-                    url: url
-                }).then(function (response) {
-                  console.log(response.data)
-                    self.commune.name = response.data.name
-                    self.commune.desc = response.data.weather[0].description
-                    self.commune.icon = response.data.weather[0].icon
-                    self.commune.temp = Math.round(response.data.main.temp)
-                    self.commune.humidity = Math.round(response.data.main.humidity)
-                    self.commune.vent = response.data.wind.speed,
-                    self.commune.coord = [response.data.coord.lat,response.data.coord.lon]
-                    console.log(self.commune.coord)
-                }).catch(function (error) {
-                    console.log(error.response);
-                }).finally(function () {
-                    store.commit('setShow',true)
-                });
+                    let self = this
+                    this.axios({
+                        method: 'get',
+                        url: url
+                    })
+                        .then(function (response) {
+                             console.log(response.data.weather[0].icon)
+                            self.commune.name = response.data.name
+                            self.commune.desc = response.data.weather[0].description
+                            self.commune.temp = Math.round(response.data.main.temp)
+                            self.commune.humidity = Math.round(response.data.main.humidity)
+                            self.commune.vent = response.data.wind.speed,
+                            self.commune.coord = [response.data.coord.lat,response.data.coord.lon]
+                            self.commune.min = response.data.main.temp_min;
+                            self.commune.max = response.data.main.temp_max;
+                            if(response.data.weather[0].icon ==="01n"){
+                                self.commune.icon ="https://cdn2.iconfinder.com/data/icons/crystalproject/crystal_project_256x256/apps/kweather.png"
+                            }
+                            else{
+                                self.commune.icon = 'http://openweathermap.org/img/wn/'+response.data.weather[0].icon+'@2x.png'
+                            }
+                            store.commit('setShow',true)
+                        }).catch(function (error) {
+                           //console.log(error.response.data);
+                           store.commit('setShow',false)
+                            self.snackbar = true
+                         }).finally(function () {
+
+                    });
+
+
+
+
 
             }
         },
@@ -133,5 +172,11 @@
 </script>
 
 <style scoped>
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .2s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
+    }
 
 </style>
